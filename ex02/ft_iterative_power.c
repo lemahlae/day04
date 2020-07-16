43a7a07b5550a985899f98532de20744c7e2a4dc
lemahlae <lemahlae@student.wethinkcode.co.za>
2020-07-16 13:31:11 +0200

    day04 commit

---
+++ ex02/ft_iterative_power.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lemahlae <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/16 13:28:18 by lemahlae          #+#    #+#             */
+/*   Updated: 2020/07/16 13:29:12 by lemahlae         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_iterative_power(int nb, int power)
+{
+	int	result;
+
+	result = 1;
+	if (power < 0)
+		return (0);
+	if (power == 0)
+		return (1)
+	while (power)
+	{
+		result *= nb;
+		power--;
+	}
+	return (result);
+}
