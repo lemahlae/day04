9818c5e5e322588d7fe8bf77e1049c46b6e673c2
lemahlae <lemahlae@student.wethinkcode.co.za>
2020-07-16 13:09:23 +0200

    Day04 commit

---
+++ ex00/ft_iterative_factorial.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lemahlae <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/16 12:53:34 by lemahlae          #+#    #+#             */
+/*   Updated: 2020/07/16 13:00:10 by lemahlae         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_iterative_factorial(int nb)
+{
+	int	result;
+	int	i;
+
+	i = 1;
+	result = 1;
+	if ((nb <= 0) || (nb > 12))
+		return (0);
+	if (nb == 1)
+		return (1);
+	while (i <= nb)
+	{
+		result = i * result;
+		i++;
+	}
+	return (result);
+}
